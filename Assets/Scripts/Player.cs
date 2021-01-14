5a68ce7e66d29a6044a77fce2060989d7955974a
Jaden Balogh <jadenbalogh@gmail.com>
2021-01-13 17:28:01 -0800

    Add movement event and change to velocity

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,21 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Player : MonoBehaviour
 {
     private const float ANIM_LOCK_DURATION = 0.05f;
 
+    [System.Serializable] public class PositionChangedEvent : UnityEvent<Vector2> {}
+
     [SerializeField] private float moveSpeed = 1f;
 
     private bool isAnimLocked;
+    private Vector3 prevFramePosition;
+
     private YieldInstruction animLockInstruction;
     private Animator animator;
+    private new Rigidbody2D rigidbody2D;
+
+    private PositionChangedEvent onPositionChanged = new PositionChangedEvent();
 
     void Awake()
     {
         animLockInstruction = new WaitForSeconds(ANIM_LOCK_DURATION);
         animator = GetComponent<Animator>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     void Update()
@@ -26,7 +35,13 @@ public class Player : MonoBehaviour
 
         UpdateMoveAnimations(inputH, inputV);
 
-        transform.position += inputDir * moveSpeed * Time.deltaTime;
+        if (transform.position != prevFramePosition)
+        {
+            onPositionChanged.Invoke(transform.position);
+        }
+
+        prevFramePosition = transform.position;
+        rigidbody2D.velocity = inputDir * moveSpeed;
     }
 
     private void UpdateMoveAnimations(float inputH, float inputV)
