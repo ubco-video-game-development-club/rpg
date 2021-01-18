9070a677da3edc2642d2c396daae11e61cd8a847
Kurtis Knodel <kurtisknodel@gmail.com>
2021-01-18 12:12:33 -0800

    Added basic window functionality

---
+++ Assets/Scripts/Editor/DialogueGraphWindow.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Architect.Dialogue
+{
+	public class DialogueGraphWindow : EditorWindow
+	{
+		public Vector2 CentreOfWindow { get { return this.position.size / 2.0f; } }
+		private DialogueGraph selectedGraph = null;
+
+		void OnEnable()
+		{
+			UpdateSelectedGraph();
+		}
+
+		void OnGUI()
+		{
+			if(selectedGraph == null)
+			{
+				ShowMessage("Select a dialogue graph to begin editing.");
+				return;
+			}
+
+			Vector2 offset = CentreOfWindow;
+			selectedGraph.Draw(offset);
+			selectedGraph.ProcessEvents(Event.current);
+		}
+
+		void OnSelectionChange()
+		{
+			UpdateSelectedGraph();
+		}
+
+		private void UpdateSelectedGraph()
+		{
+			if(Selection.activeObject is DialogueGraph)
+			{
+				selectedGraph = Selection.activeObject as DialogueGraph;
+			} else 
+			{
+				selectedGraph = null;
+			}
+		}
+
+		private void ShowMessage(string message)
+		{
+			Vector2 position = CentreOfWindow;
+			
+			GUIContent content = new GUIContent(message);
+			Vector2 size = GUI.skin.label.CalcSize(content);			
+
+			position.x -= size.x / 2.0f; //Centre the text
+			GUI.Label(new Rect(position, size), message);
+		}
+
+		[MenuItem("Window/Dialogue Graph")]
+		public static void CreateWindow()
+		{
+			DialogueGraphWindow window = GetWindow<DialogueGraphWindow>();
+			window.titleContent = new GUIContent("Dialogue Graph");
+		}
+	}
+}
