41038d0c244c528c7935ecdc19df59efbc06e83c
Kurtis Knodel <kurtisknodel@gmail.com>
2021-01-18 17:56:01 -0800

    Created entry and exit nodes

--- Assets/Scripts/Dialogue Graph/DialogueGraph.cs
+++ Assets/Scripts/Dialogue Graph/DialogueGraph.cs
@@ -7,16 +7,39 @@ namespace Architect.Dialogue
 	[CreateAssetMenu(fileName = "Dialogue Graph", menuName = "Dialogue Graph", order = 1)]
 	public class DialogueGraph : ScriptableObject
 	{
+		private static readonly Color entryColour = new Color(192.0f / 255.0f, 216.0f / 255.0f, 227.0f / 255.0f);
+		private static readonly Color exitColour = new Color(225.0f / 255.0f, 138.0f / 255.0f, 122.0f / 255.0f);
+
 		public List<DialogueGraphNode> nodes;
+		[System.NonSerialized] private DialogueGraphNode exitNode = new DialogueGraphNode("Exit", new Vector2(200, 0));
 
 		public void Draw(Vector2 offset)
 		{
-			
+			//Draw entry node
+			GUI.color = entryColour;
+			nodes[0].Draw(offset);
+			GUI.color = Color.white;
+
+			//Draw other nodes
+			for(int i = 1; i < nodes.Count; i++)
+			{
+				nodes[i].Draw(offset);
+			}
+
+			//Draw exit node
+			GUI.color = exitColour;
+			exitNode.Draw(offset);
+			GUI.color = Color.white;
 		}
 
 		public void ProcessEvents(Event e)
 		{
+			for(int i = 0; i < nodes.Count; i++)
+			{
+				nodes[i].ProcessEvents(e);
+			}
 
+			exitNode.ProcessEvents(e);
 		}
 	}
 }
--- Assets/Scripts/Dialogue Graph/DialogueGraphNode.cs
+++ Assets/Scripts/Dialogue Graph/DialogueGraphNode.cs
@@ -7,15 +7,56 @@ namespace Architect.Dialogue
 	[System.Serializable]
 	public class DialogueGraphNode
 	{
+		private static readonly Vector2 nodeSize = new Vector2(200, 50);
+
 		public string name;
 		public Vector2 position;
 		public List<DialogueGraphTransition> transitions;
+		private Rect displayRect;
+		private bool isSelected = false;
 
 		public DialogueGraphNode(string name, Vector2 position)
 		{
 			this.name = name;
 			this.position = position;
 			transitions = new List<DialogueGraphTransition>();
+		}
+
+		public void Draw(Vector2 offset)
+		{
+			Vector2 pos = position - nodeSize / 2.0f;
+			displayRect = new Rect(pos + offset, nodeSize);
+			GUI.Box(displayRect, name, GUI.skin.button);
+		}
+
+		public void ProcessEvents(Event e)
+		{
+			switch(e.type)
+			{
+				case EventType.MouseDown:
+					if(e.button == 0 && displayRect.Contains(e.mousePosition))
+					{
+						isSelected = true;
+						GUI.changed = true;
+					}
+					break;
+				case EventType.MouseUp:
+					if(e.button == 0)
+					{
+						isSelected = false;
+						GUI.changed = true;
+					}
+					break;
+				case EventType.MouseDrag:
+					if(isSelected)
+					{
+						position += e.delta;
+						GUI.changed = true;
+					}
+					break;
+				default:
+					break;
+			}
 		}
 	}
 }
