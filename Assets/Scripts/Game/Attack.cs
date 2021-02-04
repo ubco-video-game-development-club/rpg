e1c01ba8d8fa92a4fe3f97072dea62c6256ae83d
Jaden Balogh <jadenbalogh@gmail.com>
2021-02-03 21:29:24 -0800

    Update folder structure

--- Assets/Scripts/Game/Attack.cs
+++ Assets/Scripts/Game/Attack.cs
@@ -8,6 +8,9 @@ public class Attack : ScriptableObject
     [SerializeField] private float cooldown = 1f;
     public float Cooldown { get { return cooldown; } }
 
+    [SerializeField] private int damage = 1;
+    public int Damage { get { return damage; } }
+
     private bool enabled = true;
     public bool Enabled
     {
