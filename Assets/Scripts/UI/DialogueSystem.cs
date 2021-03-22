6cec94dbbcd6018412dd889428810b0965775892
Kurtis Knodel <kurtisknodel@gmail.com>
2021-03-22 00:40:39 -0700

    Added dummy button creation

--- Assets/Scripts/UI/DialogueSystem.cs
+++ Assets/Scripts/UI/DialogueSystem.cs
@@ -11,26 +11,62 @@ namespace Dialogue
         [SerializeField] private Image dialoguePortrait;
         [SerializeField] private TMPro.TextMeshProUGUI dialogueName;
         [SerializeField] private TMPro.TextMeshProUGUI dialogueText;
+        [SerializeField] private Transform dialogueButtons;
         private YieldInstruction letterCooldown = new WaitForSeconds(0.05f);
+        private DialogueGraph currentGraph;
 
         public void BeginDialogue(Sprite portrait, string name, DialogueGraph graph)
         {
+            currentGraph = graph;
+
             dialogueUI.SetActive(true);
             dialoguePortrait.sprite = portrait;
             dialogueName.text = name;
             
-            DialogueGraphNode entry = graph.nodes[0];
-            StartCoroutine(ShowDialogue(entry.body));
+            StartCoroutine(ShowDialogue(0));
         }
 
-        private IEnumerator ShowDialogue(string dialogue)
+        private DialogueGraphNode[] GetTransitionsFor(DialogueGraph graph, int node)
         {
+            LinkedList<DialogueGraphNode> transitionsList = new LinkedList<DialogueGraphNode>();
+            foreach(DialogueGraphTransition t in graph.transitions)
+            {
+                if(t.from == node)
+                {
+                    DialogueGraphNode toNode;
+                    if(t.to < 0) toNode = currentGraph.exitNode;
+                    else toNode = graph.nodes[t.to];
+                    transitionsList.AddLast(toNode);
+                }
+            }
+
+            DialogueGraphNode[] transitions = new DialogueGraphNode[transitionsList.Count];
+            transitionsList.CopyTo(transitions, 0);
+            return transitions;
+        }
+
+        private IEnumerator ShowDialogue(int node)
+        {
+            DialogueGraphNode graphNode = currentGraph.nodes[node];
+            string dialogue = graphNode.body;
+
             int index = 0;
             while(index < dialogue.Length)
             {
                 dialogueText.text = dialogue.Substring(0, index++);
                 yield return letterCooldown;
             }
+
+            DialogueGraphNode[] transitions = GetTransitionsFor(currentGraph, node);
+            foreach(DialogueGraphNode t in transitions)
+            {
+                CreateButton(t.name);
+            }
+        }
+
+        private void CreateButton(string name)
+        {
+            Debug.Log($"Create button with name {name}");
         }
     }
 }
