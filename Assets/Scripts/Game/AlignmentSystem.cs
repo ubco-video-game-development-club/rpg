937f26808ad2c0e0aa5e60299a6782f7013ace5a
wubbadukky <adcollins.d@gmail.com>
2021-04-07 17:34:57 -0700

    Alignment system updates
    Minor changes to how the 3 alignment values are stored.

--- Assets/Scripts/Game/AlignmentSystem.cs
+++ Assets/Scripts/Game/AlignmentSystem.cs
@@ -5,8 +5,12 @@ using System;
 using UnityEngine.Events;
 
 public class AlignmentSystem : MonoBehaviour
-{    
+{
     public UnityEvent<int> onAligneChanged;
+    [SerializeField] private float Morals = 0F;
+    [SerializeField] private float Leanings = 0F;
+    [SerializeField] private float Sexiness = 0F;
+
     void Start(){
         onAligneChanged.AddListener(UpdateMorals);
         onAligneChanged.AddListener(UpdateLeaning);
@@ -19,12 +23,15 @@ public class AlignmentSystem : MonoBehaviour
     }
     public void UpdateMorals(int points){
         Debug.Log("Update Morals " + points);
+        //Morals = Morals + points;
     }
         public void UpdateLeaning(int points){
         Debug.Log("Update Leanings " + points);
+        //Leanings = Leanings + points;
     }
         public void UpdateSexiness(int points){
         Debug.Log("Update Sexiness " + points);
+        //Sexiness = Sexiness + points;
     }
     public void TestAddPoints(){
         onAligneChanged.Invoke(5);
