0125ec22fe8b576d8c0bfb5da5b8d4e1395d17d4
Kurtis Knodel <kurtisknodel@gmail.com>
2021-04-25 15:29:44 -0700

    Added BehaviourTreeNodeCreator to create behaviour tree nodes

---
+++ Assets/Scripts/Tools/Behaviour Tree/BehaviourTreeNodeCreator.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace BehaviourTree
+{
+    public enum BehaviourTreeNodeType
+    {
+        SelectorNode,
+        SequenceNode,
+    }
+
+    public static class BehaviourTreeNodeCreator
+    {
+        public static BehaviourTreeNode Create(BehaviourTreeNodeType type)
+        {
+            switch(type)
+            {
+                case BehaviourTreeNodeType.SelectorNode:
+                    return new SelectorNode();
+                case BehaviourTreeNodeType.SequenceNode:
+                    return new SequenceNode();
+                default:
+                    Debug.LogError($"Unimplemented node type: {type}");
+                    return null;
+            }
+        }
+    }
+}
