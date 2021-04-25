1ee1f027db29b7fc8f1f5916b16fd7a7f9eca591
Kurtis Knodel <kurtisknodel@gmail.com>
2021-04-25 13:12:18 -0700

    Added sequence node

---
+++ Assets/Scripts/Tools/Behaviour Tree/SequenceNode.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BehaviourTree
+{
+    public class SequenceNode : BehaviourTreeNode
+    {
+        public override NodeStatus Tick(Tree<BehaviourTreeNode>.Node self)
+        {
+            for (int i = 0; i < self.ChildCount; i++)
+            {
+                Tree<BehaviourTreeNode>.Node child = self.GetChild(i);
+                NodeStatus childStatus = child.Element.Tick(child);
+                if (childStatus != NodeStatus.Success) return childStatus;
+            }
+
+            return NodeStatus.Success;
+        }
+    }
+}
