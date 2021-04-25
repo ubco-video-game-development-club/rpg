0b2261ca10b9448dc83ec5fc3a05603cde97d00b
Kurtis Knodel <kurtisknodel@gmail.com>
2021-04-25 13:03:29 -0700

    Created behaviour tree asset

---
+++ Assets/Scripts/Tools/Behaviour Tree/BehaviourTree.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BehaviourTree
+{
+    [CreateAssetMenu(fileName = "Behaviour Tree", menuName = "Behaviour Tree", order = 65)]
+    public class BehaviourTree : ScriptableObject
+    {
+        public Tree<BehaviourTreeNode> tree;
+    }
+
+    public enum NodeStatus
+    {
+        Running,
+        Success,
+        Failure
+    }
+
+    public abstract class BehaviourTreeNode
+    {
+        private Dictionary<string, VariableProperty> properties;
+
+        public void AddProperty(VariableProperty property)
+        {
+            if(properties.ContainsKey(property.Name)) return;
+
+            properties.Add(property.Name, property);
+        }
+
+        public VariableProperty GetProperty(string name)
+        {
+            if(!properties.ContainsKey(name)) return null;
+            return properties[name];
+        }
+
+        public bool RemoveProperty(string name) => properties.Remove(name);
+
+        public abstract NodeStatus Tick(Tree<BehaviourTreeNode>.Node self);
+    }
+}
