43ec287980fd04261ca427b5c715550d72220fb8
Jaden Balogh <jadenbalogh@gmail.com>
2021-05-18 20:58:58 -0700

    Add example active ability

--- Assets/Scripts/Game/Leveling/ActiveLevelUpOption.cs
+++ Assets/Scripts/Game/Leveling/ActiveLevelUpOption.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 namespace RPG
 {
+    [CreateAssetMenu(fileName = "New ActiveAbility", menuName = "LevelUp Options/Active Ability", order = 60)]
     public class ActiveLevelUpOption : LevelUpOption
     {
         [SerializeField] private Action ability;
