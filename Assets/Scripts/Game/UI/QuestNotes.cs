6584712221edc124b6c25c41eeb6726e65b6ad6b
Kurtis Knodel <kurtisknodel@gmail.com>
2021-05-24 12:15:28 -0700

    Added note selection

--- Assets/Scripts/Game/UI/QuestNotes.cs
+++ Assets/Scripts/Game/UI/QuestNotes.cs
@@ -1,9 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class QuestNotes : MonoBehaviour
+namespace RPG
 {
-    [SerializeField] private Transform notesListContent;
-    [SerializeField] private TMPro.TextMeshProUGUI noteContent;
+    public class QuestNotes : MonoBehaviour
+    {
+        [SerializeField] private RectTransform notesListContent;
+        [SerializeField] private TMPro.TextMeshProUGUI noteContent;
+        [SerializeField] private Button noteSelectButton;
+
+        void Start()
+        {
+            QuestSystem questSystem = GameManager.QuestSystem;
+            int noteCount = questSystem.NoteCount;
+            float buttonHeight = noteSelectButton.GetComponent<RectTransform>().sizeDelta.y + 5.0f;
+            notesListContent.sizeDelta = new Vector2(notesListContent.sizeDelta.x, noteCount * buttonHeight + 10.0f);
+
+            for(int i = 0; i < noteCount; i++)
+            {
+                QuestSystem.Note note = questSystem.GetNote(i);
+                Button button = Instantiate(noteSelectButton, notesListContent);
+                button.GetComponent<RectTransform>().anchoredPosition = new Vector2(10, -(buttonHeight * i + 10.0f));
+                button.GetComponentInChildren<TMPro.TextMeshProUGUI>().SetText(note.name);
+                
+                int index = i;
+                button.onClick.AddListener(delegate
+                {
+                    SelectNote(index);
+                });
+            }
+
+            if(noteCount > 0) SelectNote(0);
+        }
+
+        private void SelectNote(int index)
+        {
+            QuestSystem.Note note = GameManager.QuestSystem.GetNote(index);
+            noteContent.SetText(note.content);
+        }
+    }
 }
