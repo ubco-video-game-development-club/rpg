c52c40efbb9989ca0bd9365c5bc556887551e17d
Kurtis Knodel <kurtisknodel@gmail.com>
2021-05-24 11:19:35 -0700

    Created quest note prefabs

---
+++ Assets/Scripts/Game/UI/QuestNotes.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestNotes : MonoBehaviour
+{
+    [SerializeField] private Transform notesListContent;
+    [SerializeField] private TMPro.TextMeshProUGUI noteContent;
+}
