4dad868ec570d486df7fbc5460df9e370c233536
Jaden Balogh <jadenbalogh@gmail.com>
2021-05-25 17:32:13 -0700

    Add example code

--- Assets/Scripts/Game/AlignmentSystem.cs
+++ Assets/Scripts/Game/AlignmentSystem.cs
@@ -4,36 +4,65 @@ using UnityEngine;
 using System;
 using UnityEngine.Events;
 
-public class AlignmentSystem : MonoBehaviour
+namespace RPG
 {
-    public UnityEvent<int> onAligneChanged;
-    [SerializeField] private float Morals = 0F;
-    [SerializeField] private float Leanings = 0F;
-    [SerializeField] private float Sexiness = 0F;
+    public class AlignmentSystem : MonoBehaviour
+    {
+        public UnityEvent<int> onAligneChanged;
+        [SerializeField] private float Morals = 0F;
+        [SerializeField] private float Leanings = 0F;
+        [SerializeField] private float Sexiness = 0F;
 
-    void Start(){
-        onAligneChanged.AddListener(UpdateMorals);
-        onAligneChanged.AddListener(UpdateLeaning);
-        onAligneChanged.AddListener(UpdateSexiness);
-    }
-    void Update(){
-        if(Input.GetKeyDown(KeyCode.Space)){
-            TestAddPoints();
+        // Add new property names to PropertyName.cs
+        private int MyHealth
+        {
+            get => player.GetProperty<int>(PropertyName.Health);
+            set => player.SetProperty<int>(PropertyName.Health, value);
         }
-    }
-    public void UpdateMorals(int points){
-        Debug.Log("Update Morals " + points);
-        //Morals = Morals + points;
-    }
-        public void UpdateLeaning(int points){
-        Debug.Log("Update Leanings " + points);
-        //Leanings = Leanings + points;
-    }
-        public void UpdateSexiness(int points){
-        Debug.Log("Update Sexiness " + points);
-        //Sexiness = Sexiness + points;
-    }
-    public void TestAddPoints(){
-        onAligneChanged.Invoke(5);
+
+        private Player player;
+
+        void Start()
+        {
+            player = GameManager.Player;
+
+            onAligneChanged.AddListener(UpdateMorals);
+            onAligneChanged.AddListener(UpdateLeaning);
+            onAligneChanged.AddListener(UpdateSexiness);
+
+            // Example code (old)
+            player.SetProperty<int>(PropertyName.Health, 3);
+            int health = player.GetProperty<int>(PropertyName.Health);
+
+            // Example code (new)
+            MyHealth = 3;
+            int tmpHealth = MyHealth;
+        }
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                TestAddPoints();
+            }
+        }
+        public void UpdateMorals(int points)
+        {
+            Debug.Log("Update Morals " + points);
+            //Morals = Morals + points;
+        }
+        public void UpdateLeaning(int points)
+        {
+            Debug.Log("Update Leanings " + points);
+            //Leanings = Leanings + points;
+        }
+        public void UpdateSexiness(int points)
+        {
+            Debug.Log("Update Sexiness " + points);
+            //Sexiness = Sexiness + points;
+        }
+        public void TestAddPoints()
+        {
+            onAligneChanged.Invoke(5);
+        }
     }
 }
