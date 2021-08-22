600b569533f740bf19bbd87493c5efcfaba4d4d1
Jaden Balogh <jadenbalogh@gmail.com>
2021-08-21 19:46:45 -0700

    Add agent properties

--- Assets/Scripts/Tools/Behaviour Tree/Agent.cs
+++ Assets/Scripts/Tools/Behaviour Tree/Agent.cs
@@ -1,22 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using RPG;
 
 namespace BehaviourTree
 {
     public class Agent : MonoBehaviour
     {
         [SerializeField] private BehaviourTree behaviourTree;
+
+        public Dictionary<string, object> Properties { get; private set; }
+
         private Tree<Behaviour>.Node root;
 
         void Awake()
         {
             root = behaviourTree.Root;
+            Properties = new Dictionary<string, object>();
         }
 
         void Update()
         {
             root.Element.Tick(root, this);
+        }
+
+        public void SetProperty(string name, object property)
+        {
+            Properties[name] = property;
+        }
+
+        public object GetProperty(string name)
+        {
+            return HasProperty(name) ? Properties[name] : null;
+        }
+
+        public bool HasProperty(string name)
+        {
+            return Properties.ContainsKey(name);
         }
     }
 }
