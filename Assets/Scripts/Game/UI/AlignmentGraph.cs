2a2958bf3607d865e687fdfd5b1545c16b6e302c
Jaden Balogh <jadenbalogh@gmail.com>
2021-09-04 22:48:10 -0700

    Update AlignmentGraph to manage one point instance rather than creating and destroying points

--- Assets/Scripts/Game/UI/AlignmentGraph.cs
+++ Assets/Scripts/Game/UI/AlignmentGraph.cs
@@ -7,46 +7,18 @@ namespace RPG
 {
     public class AlignmentGraph : MonoBehaviour
     {
-        private RectTransform GraphContainer;
-
-        [SerializeField] private Sprite point;
-
-        private void Awake()
-        {
-            GraphContainer = transform.Find("GraphContainer").GetComponent<RectTransform>();
-        }
+        [SerializeField] private RectTransform point;
 
         private void Start()
         {
-            GameManager.AlignmentSystem.OnAlignmentChanged.AddListener(ShowGraph);
+            GameManager.AlignmentSystem.OnAlignmentChanged.AddListener(UpdatePoint);
         }
 
-        private void CreatePoint(Vector2 anchoredPosition)
-        {
-            if (GameObject.Find("point") != null)
-            {
-                Destroy(GameObject.Find("point"));
-            }
-
-            GameObject NewPoint = new GameObject("point", typeof(Image));
-
-            NewPoint.transform.SetParent(GraphContainer, false);
-            NewPoint.GetComponent<Image>().sprite = point;
-
-            RectTransform rectTransform = NewPoint.GetComponent<RectTransform>();
-
-            rectTransform.anchoredPosition = anchoredPosition;
-            rectTransform.sizeDelta = new Vector2(11, 11);
-            rectTransform.anchorMin = new Vector2(0, 0);
-            rectTransform.anchorMax = new Vector2(0, 0);
-        }
-
-        private void ShowGraph(float Morals, float Leanings, float Sexiness)
+        private void UpdatePoint(float Morals, float Leanings, float Sexiness)
         {
             float xPosition = (Morals * 50) + 50;
             float yPosition = (Leanings * 50) + 50;
-
-            CreatePoint(new Vector2(xPosition, yPosition));
+            point.anchoredPosition = new Vector2(xPosition, yPosition);
         }
     }
 }
