0d59ccfadd59e467c736ca95387bfc338ea27efb
Jaden Balogh <jadenbalogh@gmail.com>
2021-10-04 02:02:40 -0700

    Implement array-based VariableProperty type

--- Assets/Scripts/Editor/BehaviourTreeInspector.cs
+++ Assets/Scripts/Editor/BehaviourTreeInspector.cs
@@ -10,6 +10,10 @@ namespace BehaviourTree
     {
         private BehaviourTree behaviourTree;
 
+        private bool isArrayExpanded = false;
+        private int arrayLength = 0;
+        private object[] arr;
+
         void OnEnable()
         {
             behaviourTree = serializedObject.targetObject as BehaviourTree;
@@ -21,6 +25,8 @@ namespace BehaviourTree
 
             Behaviour node = behaviourTree.selectedNode;
             if (node == null) return;
+
+            ClearArrayData();
 
             string name = node.Node.GetType().Name;
             GUILayout.Label(name);
@@ -36,6 +42,12 @@ namespace BehaviourTree
 
         private void DisplayProperty(string name, VariableProperty property)
         {
+            if (property.PropertyType == VariableProperty.Type.Array)
+            {
+                DisplayArray(name, property);
+                return;
+            }
+
             GUILayout.BeginHorizontal();
             GUILayout.Label(name);
 
@@ -59,6 +71,119 @@ namespace BehaviourTree
             }
 
             GUILayout.EndHorizontal();
+        }
+
+        private void DisplayArray(string name, VariableProperty property)
+        {
+            int initialLength = arrayLength;
+            if (arr == null)
+            {
+                object[] propArr = property.GetArray();
+                arr = new object[propArr.Length];
+                for (int i = 0; i < propArr.Length; i++)
+                {
+                    arr[i] = propArr[i];
+                }
+                arrayLength = propArr.Length;
+            }
+
+            GUILayout.BeginHorizontal();
+            isArrayExpanded = EditorGUILayout.Foldout(isArrayExpanded, name);
+            arrayLength = EditorGUILayout.IntField(arrayLength);
+            GUILayout.EndHorizontal();
+
+            if (arrayLength != initialLength)
+            {
+                object[] tmp = new object[arrayLength];
+                for (int i = 0; i < arrayLength; i++)
+                {
+                    if (i < arr.Length)
+                    {
+                        tmp[i] = arr[i];
+                    }
+                    else
+                    {
+                        switch (property.GetArrayType())
+                        {
+                            case VariableProperty.Type.Boolean:
+                                tmp[i] = false;
+                                break;
+                            case VariableProperty.Type.Number:
+                                tmp[i] = 0.0;
+                                break;
+                            case VariableProperty.Type.String:
+                                tmp[i] = "";
+                                break;
+                            case VariableProperty.Type.Object:
+                                tmp[i] = null;
+                                break;
+                            case VariableProperty.Type.Vector:
+                                tmp[i] = Vector2.zero;
+                                break;
+                        }
+                    }
+                }
+                arr = tmp;
+            }
+
+            if (isArrayExpanded)
+            {
+                GUILayout.Space(2);
+                GUILayout.BeginHorizontal();
+                GUILayout.Space(20);
+                GUILayout.BeginVertical();
+
+                for (int i = 0; i < arrayLength; i++)
+                {
+                    GUILayout.BeginHorizontal();
+                    GUILayout.Label("Element " + i);
+                    switch (property.GetArrayType())
+                    {
+                        case VariableProperty.Type.Boolean:
+                            arr[i] = GUILayout.Toggle((bool)arr[i], GUIContent.none);
+                            break;
+                        case VariableProperty.Type.Number:
+                            arr[i] = EditorGUILayout.DoubleField((double)arr[i]);
+                            break;
+                        case VariableProperty.Type.String:
+                            arr[i] = GUILayout.TextField((string)arr[i]);
+                            break;
+                        case VariableProperty.Type.Object:
+                            arr[i] = EditorGUILayout.ObjectField((Object)arr[i], property.GetObjectType(), true);
+                            break;
+                        case VariableProperty.Type.Vector:
+                            arr[i] = EditorGUILayout.Vector2Field("", (Vector2)arr[i]);
+                            break;
+                    }
+                    GUILayout.EndHorizontal();
+                }
+
+                GUILayout.EndVertical();
+                GUILayout.EndHorizontal();
+            }
+
+            property.Set(arr);
+        }
+
+        private void ClearArrayData()
+        {
+            bool hasArrayProp = false;
+            Behaviour node = behaviourTree.selectedNode;
+            foreach (VariableProperty prop in node.Properties.Values)
+            {
+                if (prop.PropertyType == VariableProperty.Type.Array)
+                {
+                    hasArrayProp = true;
+                    break;
+                }
+            }
+
+            if (!hasArrayProp)
+            {
+                isArrayExpanded = false;
+                arrayLength = 0;
+                arr = null;
+            }
         }
     }
 }
--- Assets/Scripts/Utils/VariableProperty.cs
+++ Assets/Scripts/Utils/VariableProperty.cs
@@ -16,17 +16,40 @@ public class VariableProperty
 
     [SerializeField] private Value value;
     [SerializeField] private System.Type oType;
+    [SerializeField] private Type aType;
 
     public VariableProperty(Type type)
     {
+        if (type == Type.Object) Debug.LogError("ERROR: Attempting to use Object-based VariableProperty constructor with no Object type! You must specify the Object type!");
+        if (type == Type.Array) Debug.LogError("ERROR: Attempting to use Array-based VariableProperty constructor with no Array type! You must specify the Array type!");
         PropertyType = type;
         value = new Value();
     }
 
-    public VariableProperty(System.Type objectType)
+    public VariableProperty(Type type, System.Type objectType)
     {
+        if (type != Type.Object) Debug.LogError("ERROR: Attempting to use Object-based VariableProperty constructor with non-Object type!");
         PropertyType = Type.Object;
         oType = objectType;
+        value = new Value();
+    }
+
+    public VariableProperty(Type type, Type arrayType)
+    {
+        if (type != Type.Array) Debug.LogError("ERROR: Attempting to use Array-based VariableProperty constructor with non-Array type!");
+        if (arrayType == Type.Object) Debug.LogError("ERROR: Attempting to use Array-of-Objects-based VariableProperty constructor with no Object type! You must specify the Object type!");
+        if (arrayType == Type.Array) Debug.LogError("ERROR: VariableProperty Array type cannot have nested Arrays!");
+        PropertyType = Type.Array;
+        aType = arrayType;
+        value = new Value();
+    }
+
+    public VariableProperty(Type type, Type arrayType, System.Type objectType)
+    {
+        if (type != Type.Array) Debug.LogError("ERROR: Attempting to use Array-based VariableProperty constructor with non-Array type!");
+        if (arrayType == Type.Array) Debug.LogError("ERROR: VariableProperty Array type cannot have nested Arrays!");
+        PropertyType = Type.Array;
+        aType = arrayType;
         value = new Value();
     }
 
@@ -80,13 +103,13 @@ public class VariableProperty
 
     public void Set(UnityEngine.Object value)
     {
-        if (PropertyType != Type.Object) throw new InvalidOperationException("This property is not a object type.");
+        if (PropertyType != Type.Object) throw new InvalidOperationException("This property is not an object type.");
         this.value.o = value;
     }
 
     public Vector2 GetVector()
     {
-        if (PropertyType != Type.Vector) throw new InvalidOperationException("This property is not an vector type.");
+        if (PropertyType != Type.Vector) throw new InvalidOperationException("This property is not a vector type.");
         return value.v;
     }
 
@@ -96,13 +119,77 @@ public class VariableProperty
         this.value.v = value;
     }
 
+    public object[] GetArray()
+    {
+        if (PropertyType != Type.Array) throw new InvalidOperationException("This property is not an array type.");
+        VariablePropertyBase[] arr = value.a;
+        object[] result = new object[arr.Length];
+        for (int i = 0; i < arr.Length; i++)
+        {
+            switch (arr[i].PropertyType)
+            {
+                case VariableProperty.Type.Boolean:
+                    result[i] = arr[i].GetBoolean();
+                    break;
+                case VariableProperty.Type.Number:
+                    result[i] = arr[i].GetNumber();
+                    break;
+                case VariableProperty.Type.String:
+                    result[i] = arr[i].GetString();
+                    break;
+                case VariableProperty.Type.Object:
+                    result[i] = arr[i].GetObject();
+                    break;
+                case VariableProperty.Type.Vector:
+                    result[i] = arr[i].GetVector();
+                    break;
+            }
+        }
+        return result;
+    }
+
+    public Type GetArrayType()
+    {
+        if (PropertyType != Type.Array) throw new InvalidOperationException("This property is not an array type.");
+        return aType;
+    }
+
+    public void Set(object[] value)
+    {
+        if (PropertyType != Type.Array) throw new InvalidOperationException("This property is not an array type.");
+        this.value.a = new VariablePropertyBase[value.Length];
+        for (int i = 0; i < value.Length; i++)
+        {
+            this.value.a[i] = new VariablePropertyBase(aType);
+            switch (aType)
+            {
+                case VariableProperty.Type.Boolean:
+                    this.value.a[i].Set((bool)value[i]);
+                    break;
+                case VariableProperty.Type.Number:
+                    this.value.a[i].Set((double)value[i]);
+                    break;
+                case VariableProperty.Type.String:
+                    this.value.a[i].Set((string)value[i]);
+                    break;
+                case VariableProperty.Type.Object:
+                    this.value.a[i].Set((UnityEngine.Object)value[i]);
+                    break;
+                case VariableProperty.Type.Vector:
+                    this.value.a[i].Set((Vector2)value[i]);
+                    break;
+            }
+        }
+    }
+
     public enum Type
     {
         Boolean,
         Number,
         String,
         Object,
-        Vector
+        Vector,
+        Array
     }
 
     [System.Serializable]
@@ -113,5 +200,109 @@ public class VariableProperty
         public string s;
         public UnityEngine.Object o;
         public Vector2 v;
+        public VariablePropertyBase[] a;
+    }
+
+    [System.Serializable]
+    private class VariablePropertyBase
+    {
+        [SerializeField] private Type propertyType;
+        public Type PropertyType
+        {
+            get => propertyType;
+            private set => propertyType = value;
+        }
+
+        [SerializeField] private ValueBase value;
+        [SerializeField] private System.Type oType;
+
+        public VariablePropertyBase(Type type)
+        {
+            PropertyType = type;
+            value = new ValueBase();
+        }
+
+        public VariablePropertyBase(Type type, System.Type objectType)
+        {
+            PropertyType = Type.Object;
+            oType = objectType;
+            value = new ValueBase();
+        }
+
+        public bool GetBoolean()
+        {
+            if (PropertyType != Type.Boolean) throw new InvalidOperationException("This property is not a boolean type.");
+            return value.b;
+        }
+
+        public void Set(bool value)
+        {
+            if (PropertyType != Type.Boolean) throw new InvalidOperationException("This property is not a boolean type.");
+            this.value.b = value;
+        }
+
+        public double GetNumber()
+        {
+            if (PropertyType != Type.Number) throw new InvalidOperationException("This property is not a number type.");
+            return value.n;
+        }
+
+        public void Set(double value)
+        {
+            if (PropertyType != Type.Number) throw new InvalidOperationException("This property is not a number type.");
+            this.value.n = value;
+        }
+
+        public string GetString()
+        {
+            if (PropertyType != Type.String) throw new InvalidOperationException("This property is not a string type.");
+            return value.s;
+        }
+
+        public void Set(string value)
+        {
+            if (PropertyType != Type.String) throw new InvalidOperationException("This property is not a string type.");
+            this.value.s = value;
+        }
+
+        public UnityEngine.Object GetObject()
+        {
+            if (PropertyType != Type.Object) throw new InvalidOperationException("This property is not an object type.");
+            return value.o;
+        }
+
+        public System.Type GetObjectType()
+        {
+            if (PropertyType != Type.Object) throw new InvalidOperationException("This property is not an object type.");
+            return oType;
+        }
+
+        public void Set(UnityEngine.Object value)
+        {
+            if (PropertyType != Type.Object) throw new InvalidOperationException("This property is not an object type.");
+            this.value.o = value;
+        }
+
+        public Vector2 GetVector()
+        {
+            if (PropertyType != Type.Vector) throw new InvalidOperationException("This property is not a vector type.");
+            return value.v;
+        }
+
+        public void Set(Vector2 value)
+        {
+            if (PropertyType != Type.Vector) throw new InvalidOperationException("This property is not a vector type.");
+            this.value.v = value;
+        }
+
+        [System.Serializable]
+        private struct ValueBase
+        {
+            public bool b;
+            public double n;
+            public string s;
+            public UnityEngine.Object o;
+            public Vector2 v;
+        }
     }
 }
