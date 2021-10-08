e90a0f86c7d1b71b7e3c7096107e68be0566747c
Jaden Balogh <jadenbalogh@gmail.com>
2021-10-07 17:14:43 -0700

    Change behaviour movement to physics-based

--- Assets/Scripts/Tools/Behaviour Tree/Nodes/SimpleMoveNode.cs
+++ Assets/Scripts/Tools/Behaviour Tree/Nodes/SimpleMoveNode.cs
@@ -7,34 +7,37 @@ namespace BehaviourTree
     public class SimpleMoveNode : IBehaviourTreeNode
     {
         private const string PROP_POSITION_SRC = "position-source";
-        private const string PROP_MIN_DISTANCE = "min-distance";
+        private const string PROP_STOP_DISTANCE = "stop-distance";
         private const string PROP_MOVE_SPEED = "move-speed";
 
         public void Serialize(Behaviour behaviour)
         {
             behaviour.Properties.Add(PROP_POSITION_SRC, new VariableProperty(VariableProperty.Type.String));
-            behaviour.Properties.Add(PROP_MIN_DISTANCE, new VariableProperty(VariableProperty.Type.Number));
+            behaviour.Properties.Add(PROP_STOP_DISTANCE, new VariableProperty(VariableProperty.Type.Number));
             behaviour.Properties.Add(PROP_MOVE_SPEED, new VariableProperty(VariableProperty.Type.Number));
         }
 
         public NodeStatus Tick(Tree<Behaviour>.Node self, BehaviourObject obj)
         {
             Behaviour behaviour = self.Element;
+            Rigidbody2D rigidbody2D = ((Agent)obj).Rigidbody2D;
 
             string src = behaviour.Properties[PROP_POSITION_SRC].GetString();
             if (obj.HasProperty(src))
             {
                 Vector2 targetPosition = (Vector2)obj.GetProperty(src);
                 Vector2 currentPosition = obj.transform.position;
-                Vector2 d = currentPosition - targetPosition;
+                Vector2 d = targetPosition - currentPosition;
 
-                float minDist = (float)behaviour.Properties[PROP_MIN_DISTANCE].GetNumber();
-                if (d.sqrMagnitude > minDist * minDist)
+                float stopDist = (float)behaviour.Properties[PROP_STOP_DISTANCE].GetNumber();
+                if (d.sqrMagnitude > stopDist * stopDist)
                 {
                     float speed = (float)behaviour.Properties[PROP_MOVE_SPEED].GetNumber();
-                    obj.transform.position = Vector2.MoveTowards(currentPosition, targetPosition, speed * Time.deltaTime);
+                    rigidbody2D.velocity = d.normalized * speed;
                     return NodeStatus.Running;
                 }
+
+                rigidbody2D.velocity = Vector2.zero;
                 return NodeStatus.Success;
             }
             return NodeStatus.Failure;
