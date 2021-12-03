21708121f290964d4d2d7c3d9575683d99738e8d
Jaden Balogh <jadenbalogh@gmail.com>
2021-12-02 22:49:49 -0800

    Refactor agent enable logic

--- Assets/Scripts/Tools/Behaviour Tree/Agent.cs
+++ Assets/Scripts/Tools/Behaviour Tree/Agent.cs
@@ -13,34 +13,17 @@ namespace BehaviourTree
         public Rigidbody2D Rigidbody2D { get; private set; }
 
         private Tree<Behaviour>.Node root;
-        private bool isEnabled;
 
         protected override void Awake()
         {
             base.Awake();
-
             Rigidbody2D = GetComponent<Rigidbody2D>();
-
             root = behaviourTree.Root;
-            isEnabled = true;
         }
 
         protected void Update()
         {
-            if (isEnabled)
-            {
-                root.Element.Tick(root, this);
-            }
-        }
-
-        public void DisableBehaviours()
-        {
-            isEnabled = false;
-        }
-
-        public void EnableBehaviours()
-        {
-            isEnabled = true;
+            root.Element.Tick(root, this);
         }
     }
 }
